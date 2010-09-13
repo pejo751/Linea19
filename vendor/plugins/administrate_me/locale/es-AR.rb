cc3edc77d18611d5da3d9d76e2c61f6c297dc442
unknown <administrador@.ippi.local>
2010-09-13 15:27:12 -0300

    Correcciones varias

--- vendor/plugins/administrate_me/locale/es-AR.rb
+++ vendor/plugins/administrate_me/locale/es-AR.rb
@@ -5,7 +5,7 @@
   :views => {
     :prev => 'Anterior',
     :next => 'Siguiente',
-    :logout => 'Cerrar sesion',
+    :logout => 'Salir',
     :save_this_changes => 'Guardar estos cambios',
     :cancel => 'Cancelar',
     :add_new_record => 'Agregar nuevo',
