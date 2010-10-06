2506e71db191adbca6fc5237c9b2343f51aef973
Jorge Pereyra <jorgepereyra751@hotmail.com>
2010-10-06 12:55:53 -0300

    Filtros para documentos de legajos

--- app/controllers/admin/documentos_de_legajos_controller.rb
+++ app/controllers/admin/documentos_de_legajos_controller.rb
@@ -1,7 +1,15 @@
 class Admin::DocumentosDeLegajosController < Admin::CoreController
 	administrate_me do |admin|
 		admin.except :new, :show, :edit, :destroy
-		admin.search :descripcion
-		admin.order 'legajo_id, descripcion'
+		admin.includes :legajo
+		admin.search 'legajos.legajo'
+		#admin.search :descripcion
+		#admin.order 'legajo_id, descripcion'
+		admin.order 'legajos.nombre'
+		admin.filters do |f|
+			f.set :vencidos, :conditions => ['vencimiento < ?', Date.today]
+			f.set :vigentes, :conditions => ['vencimiento >= ?', Date.today]
+			f.set :falta_vencimiento, :conditions => ['vencimiento IS NULL']
+		end
 	end
 end
